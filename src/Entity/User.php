a299d1bbb1e1198b88be1e05021c528b40a0cd09
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-31 12:30:08 +0200

    made an entity User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,5 +4,21 @@ namespace App\Entity;
 
 class User
 {
+    private $name;
 
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
 }
